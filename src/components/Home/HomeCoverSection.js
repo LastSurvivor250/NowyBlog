4d88cea3ad44dd4716d604f07b483b0af305d51e
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-11-09 08:59:45 +0100

    mainpost

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -24,7 +24,7 @@ const HomeCoverSection = ({ blogs }) => {
           blurDataURL={blog.image.blurhashDataUrl}
           alt={blog.title}
           fill
-          className="h-90% w-90%  object-bottom object-cover rounded-2xl -z-10 "
+          className="h-90% w-90%  object-bottom object-cover rounded-xl -z-10 "
           sizes="80vw"
           priority
         />

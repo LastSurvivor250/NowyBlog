4f307a6288ab275fc7547ee00ce5d6cee66dde6b
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-01-20 21:55:31 +0100

    changesmanyx

--- src/components/Blog/BlogLayoutThree.js
+++ src/components/Blog/BlogLayoutThree.js
@@ -6,7 +6,7 @@ import React from "react";
 const BlogLayoutThree = ({ blog }) => {
   return (
     <div className="group flex flex-col items-center text-dark dark:text-light">
-      <Link href={blog.url} className="h-full rounded-xl overflow-hidden">
+      <Link href={blog.url} className="h-full  overflow-hidden">
         <Image
           src={blog.image.filePath.replace("../public", "")}
           placeholder="blur"
@@ -14,7 +14,7 @@ const BlogLayoutThree = ({ blog }) => {
           alt={blog.title}
           width={blog.image.width}
           height={blog.image.height}
-          className=" aspect-[4/3] w-full h-full object-cover object-center  group-hover:scale-105 transition-all ease duration-300 "
+          className=" aspect-[4/3] h-full object-cover object-center  group-hover:scale-105 transition-all ease duration-300 "
           sizes="(max-width: 640px) 100vw,(max-width: 1024px) 50vw, 33vw"
         />
       </Link>

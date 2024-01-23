ee6ae3f9132656e3ddb5a47772a8b5a2b7c5d181
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-01-23 14:57:23 +0100

    changesmanyx

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -22,7 +22,7 @@ const HomeCoverSection = ({ blogs }) => {
           blurDataURL={blog.image.blurhashDataUrl}
           alt={blog.title}
           fill
-          className="width-100% object-bottom object-cover rounded-medium -z-1 "
+          className=" object-bottom object-cover rounded-medium -z-1 "
           sizes="100vw"
           priority
         />

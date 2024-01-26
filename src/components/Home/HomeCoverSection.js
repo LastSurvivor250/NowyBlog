d4cd74a033b9c257bc722903bab8b7a1d55f6e4e
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-01-26 14:05:12 +0100

    changesmanyx

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -50,7 +50,7 @@ const HomeCoverSection = ({ blogs }) => {
                 <BlogLayoutFour blog={sortedBlogs[8]} />
               </article>
               <article className="col-span-1 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutFour blog={sortedBlogs[3]} />
+                <BlogLayoutFour blog={sortedBlogs[8]} />
               </article>
               <br /> <br /> <br /> <br /> <br /> <br /> <br />
             </div>

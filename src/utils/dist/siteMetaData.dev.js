02ff63ddf02f3a8119b4453f264a970fcd337318
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-05 19:06:23 +0100

    secondcommit

--- src/utils/dist/siteMetaData.dev.js
+++ src/utils/dist/siteMetaData.dev.js
@@ -1,14 +1,19 @@
+"use strict";
+
 // please update required information
-const siteMetadata = {
+var siteMetadata = {
   title: "Next.js Blog With Tailwind CSS and Contentlayer",
   author: "FocusedP",
   headerTitle: "Next.js Blog",
   description: "A blog created with Next.js, Tailwind.css and contentlayer.",
   language: "en-us",
-  theme: "system", // system, dark or light
-  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  theme: "system",
+  // system, dark or light
+  siteUrl: "https://create-blog-with-nextjs.vercel.app",
+  // your website URL
   siteLogo: "/profile-idmg.png",
-  socialBanner: "/social-banner.png", // add social banner in the public folder
+  socialBanner: "/social-banner.png",
+  // add social banner in the public folder
   email: "migasiuk250@gmail.com",
   github: "https://github.com/lastSurvivor250",
   twitter: "https://twitter.com/code_bucks",
@@ -16,7 +21,6 @@ const siteMetadata = {
   youtube: "https://youtube.com/magicmoisture",
   linkedin: "https://www.linkedin.com/in/codebucks/",
   dribbble: "https://www.dribbble.com",
-  locale: "en-US",
+  locale: "en-US"
 };
-
 module.exports = siteMetadata;

dc4a6dca3b043b2b4c8e00d995ad877b0e1ccd0c
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-19 15:58:46 +0100

    commer1

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -29,7 +29,7 @@ const FeaturedPosts = ({ blogs }) => {
               <div className="grid grid-cols-2 grid-rows-1 gap-6   sm:mt-16 space-between margin-left:170px ">
                 <article
                   className=" col-span-2 sm:col-span-1 row-span-1 relative width:72px "
-                  style={{ width: "70%", left: "0px" }}
+                  style={{ width: "70%", left: "0px", bottom: "-10px" }}
                 >
                   <BlogLayoutFour blog={sortedBlogs[9]} />
                 </article>

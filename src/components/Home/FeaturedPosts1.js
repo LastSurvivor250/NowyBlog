36a805a13cee5378820288cd21f24cd60570d5be
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-19 18:00:45 +0100

    commer1

--- src/components/Home/FeaturedPosts1.js
+++ src/components/Home/FeaturedPosts1.js
@@ -18,21 +18,25 @@ const FeaturedPosts1 = ({ blogs }) => {
       <br />
       <br />
       <br />
-      HTML
       <article className="flex flex-row items-start justify-end sm:mx-10 relative h-[164vh] sm:h-[85vh]">
         <ConnectedSite />
         <section className="w-full mt-0 margin-right:11rem margin-left:-174px sm:mt-24 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center z-10">
           <h2 className="text-center w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
             --- DESIGN CORNER ---
           </h2>
-
+          <br />
+          <br />
           <div className="grid grid-cols-2 grid-rows-3 gap-4">
             <article className="row-span-1 col-span-1">
               <BlogLayoutFour blog={sortedBlogs[9]} />
             </article>
 
             <article className="row-span-1 col-span-1">
-              <BlogLayoutFour blog={sortedBlogs[9]} />
+              <BlogLayoutFour blog={sortedBlogs[8]} />
+            </article>
+
+            <article className="row-span-1 col-span-1">
+              <BlogLayoutFour blog={sortedBlogs[6]} />
             </article>
 
             <article className="row-span-1 col-span-1">
@@ -40,15 +44,11 @@ const FeaturedPosts1 = ({ blogs }) => {
             </article>
 
             <article className="row-span-1 col-span-1">
-              <BlogLayoutFour blog={sortedBlogs[9]} />
+              <BlogLayoutFour blog={sortedBlogs[5]} />
             </article>
 
             <article className="row-span-1 col-span-1">
-              <BlogLayoutFour blog={sortedBlogs[9]} />
-            </article>
-
-            <article className="row-span-1 col-span-1">
-              <BlogLayoutFour blog={sortedBlogs[9]} />
+              <BlogLayoutFour blog={sortedBlogs[4]} />
             </article>
           </div>
         </section>

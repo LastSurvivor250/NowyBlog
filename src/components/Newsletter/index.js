c0e15d36b9448c8de1a7369c7687bad99f8d9024
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-19 15:38:35 +0100

    commer1

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -14,22 +14,24 @@ const Newsletter = () => {
   console.log(errors);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mt-6 w-full sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
-    >
-      <input
-        type="email"
-        placeholder="Enter your email"
-        {...register("email", { required: true, maxLength: 80 })}
-        className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
-      />
+    <div className="mt-6  bg-dark dark:bg-light m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-6 w-full sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
+      >
+        <input
+          type="email"
+          placeholder="Enter your email"
+          {...register("email", { required: true, maxLength: 80 })}
+          className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
+        />
 
-      <input
-        type="submit"
-        className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
-      />
-    </form>
+        <input
+          type="submit"
+          className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
+        />
+      </form>
+    </div>
   );
 };
 

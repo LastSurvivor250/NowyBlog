cc88dcd36dc995b72c8f7321d56fa9fe4e92d934
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-20 16:34:53 +0100

    secondcommit

--- src/components/Container1/index.js
+++ src/components/Container1/index.js
@@ -10,7 +10,7 @@ const Container1 = () => {
       <br />
       <br />
       <br />
-      <div className="mt-6 py-12 my-12  bg-dark2 items-end content-end  border border-solid border-light dark:bg-dark2 m-2 sm:m-10 flex flex-col  text-light dark:text-light">
+      <div className="mt-6 py-12 my-12 w-fit bg-dark2 items-end content-end  border border-solid border-light dark:bg-dark2 m-2 sm:m-10 flex flex-col  text-light dark:text-light">
         <div>
           <p className="mt-1 mb-1 mr-1 w-fit justify-end sm:min-w-[384px] flex items-stretch bg-light2 dark:bg-dark p-1 sm:p-2 rounded mx04">
             This BLOG is brought to Your By RESOLVEREVOLT. We are working on it

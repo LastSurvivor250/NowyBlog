1e9adfb85ade8d011f3f7f520528e450f844a248
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-20 16:25:13 +0100

    secondcommit

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
 import FeaturedPosts1 from "../components/Home/FeaturedPosts1";
 import FeaturedPosts2 from "../components/Home/FeaturedPosts2";
+import Container1 from "../components/Container1";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts2 blogs={allBlogs} />
+      <Container1 blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <FeaturedPosts1 blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
---
+++ src/components/Container1/index.js
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+const Container1 = () => {
+  console.log(errors);
+
+  return (
+    <div className="mt-6  bg-dark2 items-end content-end  border border-solid border-light dark:bg-dark2 m-2 sm:m-10 flex flex-col  text-light dark:text-light">
+      <div>
+        <p className="mt-1 mb-1 mr-1 w-fit justify-end sm:min-w-[384px] flex items-stretch bg-light2 dark:bg-dark p-1 sm:p-2 rounded mx04">
+          This BLOG is brought to Your By RESOLVEREVOLT. We are working on it
+          day and night to provide You only with BEST up to date content.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Container1;

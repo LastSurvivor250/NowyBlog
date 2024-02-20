f8aff62252bce4c9567e2b332c627c5d7c1754b7
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-02-20 16:34:07 +0100

    secondcommit

--- src/components/Container1/index.js
+++ src/components/Container1/index.js
@@ -5,14 +5,20 @@ const Container1 = () => {
   console.log(errors);
 
   return (
-    <div className="mt-6  bg-dark2 items-end content-end  border border-solid border-light dark:bg-dark2 m-2 sm:m-10 flex flex-col  text-light dark:text-light">
-      <div>
-        <p className="mt-1 mb-1 mr-1 w-fit justify-end sm:min-w-[384px] flex items-stretch bg-light2 dark:bg-dark p-1 sm:p-2 rounded mx04">
-          This BLOG is brought to Your By RESOLVEREVOLT. We are working on it
-          day and night to provide You only with BEST up to date content.
-        </p>
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="mt-6 py-12 my-12  bg-dark2 items-end content-end  border border-solid border-light dark:bg-dark2 m-2 sm:m-10 flex flex-col  text-light dark:text-light">
+        <div>
+          <p className="mt-1 mb-1 mr-1 w-fit justify-end sm:min-w-[384px] flex items-stretch bg-light2 dark:bg-dark p-1 sm:p-2 rounded mx04">
+            This BLOG is brought to Your By RESOLVEREVOLT. We are working on it
+            day and night to provide You only with BEST up to date content.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 

1ae5493d01a8db3451771d134e54c25ec61d4765
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-03-02 11:06:25 +0100

    commer1

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,81 +15,82 @@ const Footer = () => {
   console.log(errors);
 
   return (
-    <footer className="mt-16  bg-dark dark:bg-light m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
-      <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
+    <footer class="flex flex-col items-center bg-gradient-to-r from-sky-500 to-indigo-500 text-white mt-16 sm:mt-24 p-8 rounded-lg shadow-md">
+      <h3 class="font-medium text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
         Interesting Stories | Updates | Guides
       </h3>
-      <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
+      <p class="mt-5 px-4 text-center w-full sm:w-3/5 text-base leading-relaxed">
+        Subscribe to learn about new technology and updates. Join our community
+        of over 5000+ members to stay up to date with the latest news.
       </p>
 
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
+        class="flex items-stretch mt-6 w-fit sm:min-w-[384px] bg-white rounded-lg p-4 shadow-md"
       >
         <input
           type="email"
           placeholder="Enter your email"
           {...register("email", { required: true, maxLength: 80 })}
-          className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
+          class="w-full bg-transparent pl-2 sm:pl-0 text-gray-700 focus:border-sky-500 focus:ring-0 border-0 border-b mr-2 pb-1"
         />
-
         <input
           type="submit"
-          className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
+          value="Subscribe"
+          class="bg-indigo-500 hover:bg-indigo-700 text-white font-medium rounded px-3 sm:px-5 py-1 cursor-pointer"
         />
       </form>
-      <div className="flex items-center mt-8">
+
+      <div class="flex items-center mt-8 justify-center">
         <a
           href={siteMetadata.linkedin}
-          className="inline-block w-6 h-6 mr-4"
+          class="inline-block w-6 h-6 mr-4"
           aria-label="Reach out to me via LinkedIn"
           target="_blank"
         >
-          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+          <LinkedinIcon class="fill-current hover:scale-125 transition-all ease duration-200" />
         </a>
         <a
           href={siteMetadata.twitter}
-          className="inline-block w-6 h-6 mr-4"
+          class="inline-block w-6 h-6 mr-4"
           aria-label="Reach out to me via Twitter"
           target="_blank"
         >
-          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+          <TwitterIcon class="fill-current hover:scale-125 transition-all ease duration-200" />
         </a>
         <a
           href={siteMetadata.github}
-          className="inline-block w-6 h-6 mr-4 fill-light"
+          class="inline-block w-6 h-6 mr-4"
           aria-label="Check my profile on Github"
           target="_blank"
         >
-          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
+          <GithubIcon class="fill-current hover:scale-125 transition-all ease duration-200" />
         </a>
         <a
           href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
+          class="inline-block w-6 h-6 mr-4"
           aria-label="Check my profile on Dribbble"
           target="_blank"
         >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+          <DribbbleIcon class="fill-current hover:scale-125 transition-all ease duration-200" />
         </a>
       </div>
 
-      <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
-        <span className="text-center">
-          &copy;2024 FocusedP. All rights reserved.
+      <div class="flex flex-col md:flex-row items-center justify-between pt-6">
+        <span class="text-center text-white">
+          ©2024 FocusedP. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
-          className="text-center underline my-4 md:my-0"
+          class="text-center text-white underline mt-4 md:mt-0"
         >
           sitemap.xml
         </Link>
-        <div className="text-center">
-          Made with &hearts; by{" "}
+        <div class="text-center text-white">
+          Made with ❤️ by{" "}
           <a
             href="https://devdreaming.com"
-            className="underline"
+            class="underline text-white"
             target="_blank"
           >
             FocusedP

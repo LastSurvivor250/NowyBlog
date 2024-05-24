8c80a27b8666bbf2b352be6a1ac18877ff5b7d0f
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-05-24 20:59:35 +0200

    first commit

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -17,14 +17,16 @@ const AboutCoverSection = () => {
 
       <div className="w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16">
         <h2 className="font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left">
-          Dream Big, Work Hard, Achieve More!
+          Dream Fairly Big, Work Kinda Hard, Achieve Cool Sh**! Sounds
+          half-assed? Well, Fu** You too.
         </h2>
         <p className="font-medium capitalize mt-4 text-base">
-          This Mantra Drives My Work As A Passionate Freelancer. I Blend
-          Innovative Technology With Timeless Design For Captivating Digital
-          Experiences. Inspired By Nature And Literature, I'm A Perpetual
-          Learner Embracing Challenges. With Each Project, I Aim To Leave A
-          Lasting Impact—One Pixel At A Time.
+          As a passionate freelancer, I live by a simple mantra: "Sky's the
+          limit, but keep it real." I mix cutting-edge tech with classic design
+          to create engaging digital experiences. Inspired by nature and
+          literature, I'm always learning and ready to tackle new challenges.
+          Every project is a chance to make a lasting impact—one pixel at a
+          time.
         </p>
       </div>
     </section>

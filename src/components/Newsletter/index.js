b98a5ad92020cebe7ebff74cf2d76665fcde64f0
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-05-27 16:33:53 +0200

    first commit

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -3,18 +3,20 @@ import { X, Download } from "lucide-react";
 
 const Model = () => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm">
-      <button>
-        <X />
-      </button>
-      <h1>Download My Free Ebook</h1>
-      <p>Want To Learn How To Create Content Effectively ?</p>
-      <form>
-        <input type="email" placeholder="enter your email" required />
-        <button>
-          <Download /> Download Link
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex- justify-center items-center">
+      <div className="mt-10 flex-col gap-5 text-white">
+        <button className="place-self-end">
+          <X size={30} />
         </button>
-      </form>
+        <h1>Download My Free Ebook</h1>
+        <p>Want To Learn How To Create Content Effectively ?</p>
+        <form>
+          <input type="email" placeholder="enter your email" required />
+          <button>
+            <Download /> Download Link
+          </button>
+        </form>
+      </div>
     </div>
   );
 };

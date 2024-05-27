9318644b563fc1d9821ce21ed98f679a2ebb7e80
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-05-27 16:07:31 +0200

    first commit

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import profileCharacter from "../../../public/character.png";
-
+{
+  /* eslint-disable-next-line react/no-unescaped-entities */
+}
 const AboutCoverSection = () => {
   return (
     <section className="w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">

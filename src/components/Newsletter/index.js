d64f612f08185120a75e380990f16253c5392c69
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-06-02 19:09:11 +0200

    first commit

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -17,7 +17,7 @@ const Model = ({ onClose }) => {
       onClick={closeModal}
       className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-10"
     >
-      <div className="bg-indigo-600 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4">
+      <div className="bg-indigo-600 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 text-white">
         <button onClick={onClose} className="place-self-end">
           <X size={30} />
         </button>

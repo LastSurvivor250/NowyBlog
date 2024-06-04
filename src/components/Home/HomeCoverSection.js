4b6030a452f03e8f6352e647a56c6b63db685f07
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-06-04 22:48:16 +0200

    first commit

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -41,7 +41,7 @@ const HomeCoverSection = ({ blogs }) => {
                 <BlogLayoutFour blog={sortedBlogs[3]} />
               </article>
               <article className="col-span-1 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutFour blog={sortedBlogs[12]} />
+                <BlogLayoutFour blog={sortedBlogs[11]} />
               </article>
               <article className="col-span-1 sm:col-span-1 row-span-1 relative">
                 <BlogLayoutFour blog={sortedBlogs[4]} />

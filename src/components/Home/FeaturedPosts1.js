f063a62a777f8ce6dfdfe71c28f382760d0f9dac
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-06-04 23:37:43 +0200

    first commit

--- src/components/Home/FeaturedPosts1.js
+++ src/components/Home/FeaturedPosts1.js
@@ -50,7 +50,7 @@ const FeaturedPosts1 = ({ blogs }) => {
             </article>
 
             <article className="row-span-1 col-span-1">
-              <BlogLayoutFour blog={sortedBlogs[11]} />
+              <BlogLayoutFour blog={sortedBlogs[12]} />
             </article>
           </div>
         </section>

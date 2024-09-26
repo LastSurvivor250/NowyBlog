ede6009217a13c5e7a885cf78bd752a3c16c410a
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-09-26 19:40:59 +0200

    first commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,10 +7,10 @@ const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-sky-500 to-indigo-500 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-30 items-center justify-between">
+        <div className="flex h-50 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <div class="thematic-nav bg-gray-200 py-2 text-center">
-              <ul class="list-none p-0 m-0">
+              <ul class="list-none ">
                 <li class="inline-block mx-2">
                   <a
                     href="#"
@@ -65,14 +65,6 @@ const Navbar = () => {
                     class="no-underline text-white font-bold px-2.5 py-1 rounded-md transition duration-300 hover:bg-gray-300"
                   >
                     Store
-                  </a>
-                </li>
-                <li class="inline-block mx-2">
-                  <a
-                    href="#"
-                    class="no-underline text-white font-bold px-2.5 py-1 rounded-md transition duration-300 hover:bg-gray-300"
-                  >
-                    X
                   </a>
                 </li>
               </ul>

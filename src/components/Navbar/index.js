a1f0615d5a1fe040747d9356f5e63ef9ed560903
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2024-09-26 20:51:58 +0200

    first commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -132,7 +132,7 @@ const Navbar = () => {
                     IT
                   </a>
                 </li>
-                <li clasNames="inline-block mx-2">
+                <li clansName="inline-block mx-2">
                   <a
                     href="#"
                     className="no-underline text-white font-bold px-2.5 py-1 rounded-md transition duration-300 hover:bg-gray-300"

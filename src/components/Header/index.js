391036837a273ccc8f1501602aa0e1077f168857
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-07 12:33:39 +0100

    1st

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -86,17 +86,17 @@ const Header = () => {
       {/* Theme Switcher Button */}
       <button
         onClick={() => setMode(mode === "light" ? "dark" : "light")}
-        className={`ml-1 w-6 h-6 flex items-center justify-center rounded-full transition-all duration-300 ease-in-out ${
+        className={`ml-2 w-6 h-6 flex items-center justify-center rounded-full transition-all duration-300 ease-in-out ${
           mode === "light"
             ? "bg-gray-200 text-gray-800 hover:bg-gray-300"
-            : "bg-gray-800 text-gray-200 hover:bg-gray-700"
+            : "bg-white text-gray-800 hover:bg-gray-100"
         }`}
         aria-label="theme-switcher"
       >
         {mode === "light" ? (
           <MoonIcon className="fill-gray-800" />
         ) : (
-          <SunIcon className="fill-gray-200" />
+          <SunIcon className="fill-gray-800" />
         )}
       </button>
 

98cef1199310a2d2ae8982d8d16b7f42faafe130
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-09 12:48:56 +0100

    1st

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -23,7 +23,7 @@ const Navbar = () => {
         <div className="flex h-10 items-center justify-between">
           <div className="flex flex-1 items-center md:items-stretch md:justify-start">
             <div className="flex items-center justify-between w-full py-4 bg-gray-50 dark:bg-gray-900 rounded-lg ml-20 pl-4 shadow-sm">
-              <ul className="flex list-none space-x-6 text-lg ">
+              <ul className="flex list-none space-x-4 text-lg ">
                 <li>
                   <a
                     href="#"
@@ -119,7 +119,7 @@ const Navbar = () => {
                   </a>
                 </li>
               </ul>
-              <div className="flex list-none space-x-6">
+              <div className="flex list-none ml-auto">
                 <button
                   onClick={() => setShowModal(true)}
                   className="bg-indigo-600 hover:bg-indigo-400 px-4 py-1 shadow-lg text-lg text-white font-semibold transition-all ease-in-out"

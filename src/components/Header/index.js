b112db16010495fbd94b146424c663d4cae18c5b
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-10 11:16:55 +0100

    2nd

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -44,8 +44,9 @@ const Header = () => {
       </button>
 
       {/* Navigation Bar */}
-      <div className="flex items-center justify-around w-full py-2 mr-4 space-x-8 bg-gray-50 dark:bg-gray-900 rounded-lg  shadow-sm">
-        <nav className="hidden sm:flex items-center mr-4 space-x-8 ">
+      <div className="relative w-full">
+        {/* Nawigacja jako niezależny element */}
+        <nav className="absolute left-8 top-1/2 -translate-y-1/2 hidden sm:flex items-center space-x-4">
           <Link
             href="/"
             className="text-gray-700 hover:text-indigo-600 dark:text-white"
@@ -58,31 +59,11 @@ const Header = () => {
           >
             Home
           </Link>
-          <Link
-            href="/about"
-            className="text-gray-700 hover:text-indigo-600 dark:text-white"
-          >
-            About
-          </Link>
-          <Link
-            href="/contact"
-            className="text-gray-700 hover:text-indigo-600 dark:text-white"
-          >
-            Contact
-          </Link>
-          <Link
-            href="/services"
-            className="text-gray-700 hover:text-indigo-600 dark:text-white"
-          >
-            Services
-          </Link>
+          {/* pozostałe linki */}
         </nav>
 
-        {/* Button Group */}
-        {/* Modal */}
-
-        {/* CTA Button */}
-        <div className="ml-4">
+        {/* Przycisk jako niezależny element */}
+        <div className="absolute right-8 top-1/2 -translate-y-1/2">
           <button
             onClick={() => setShowModal(true)}
             className="bg-[#ffc107ca] hover:bg-[#FFC107] px-5 py-2 border-2 border-indigo-400 shadow-lg text-sm text-black font-semibold rounded-lg transition-transform transform hover:scale-105 ease-in-out"
@@ -90,10 +71,14 @@ const Header = () => {
             Claim Your FREE Guide!
           </button>
         </div>
+
         {showModal && (
           <Model style={{ zIndex: 999 }} onClose={() => setShowModal(false)} />
         )}
       </div>
+      {showModal && (
+        <Model style={{ zIndex: 999 }} onClose={() => setShowModal(false)} />
+      )}
     </header>
   );
 };

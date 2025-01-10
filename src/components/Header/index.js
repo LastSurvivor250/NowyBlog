4df23ac0d96bebdcf65775fc138c5d14b7240ab4
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-10 10:45:15 +0100

    2nd

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,9 @@ const Header = () => {
     setClick(!click);
   };
   return (
-    <header className="w-full flex items-center justify-between bg-gradient-to-r from-white via-gray-100 to-white dark:from-dark dark:via-gray-800 dark:to-dark shadow-md">
+    <header className="w-full   flex items-center justify-between bg-gradient-to-r from-white via-gray-100 to-white dark:from-dark dark:via-gray-800 dark:to-dark shadow-md">
+      {/* Logo Section */}
+
       {/* Hamburger Menu for Mobile */}
       <button
         className="inline-block sm:hidden z-50"
@@ -42,9 +44,8 @@ const Header = () => {
       </button>
 
       {/* Navigation Bar */}
-      <div className="flex items-center w-full">
-        {/* Links Section */}
-        <nav className="hidden sm:flex items-center space-x-8 ml-10 flex-grow">
+      <div className="flex items-center justify-around w-full py-2 mr-4 space-x-8 bg-gray-50 dark:bg-gray-900 rounded-lg  shadow-sm">
+        <nav className="hidden sm:flex items-center mr-4 space-x-8 ">
           <Link
             href="/"
             className="text-gray-700 hover:text-indigo-600 dark:text-white"
@@ -77,7 +78,10 @@ const Header = () => {
           </Link>
         </nav>
 
-        {/* Button Section */}
+        {/* Button Group */}
+        {/* Modal */}
+
+        {/* CTA Button */}
         <div className="ml-4">
           <button
             onClick={() => setShowModal(true)}

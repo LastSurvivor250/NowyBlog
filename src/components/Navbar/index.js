45eeae1bb4d7fd28c10fab0884b5293913d4e70f
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-11 14:36:37 +0100

    2nd

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -34,10 +34,10 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-700 border-b border-indigo-500">
-      <div className="container py-8">
+      <div className="container py-4">
         <div className="flex items-center justify-evenly relative">
           {/* Navigation Links */}
-          <div className="absolute left-0">
+          <div className="absolute left-164px">
             <Logo />
           </div>
 
@@ -54,7 +54,7 @@ const Navbar = () => {
           </div>
 
           {/* Social Media and Theme Switcher */}
-          <div className="absolute right-0 flex items-center space-x-2">
+          <div className="absolute right-229px flex items-center space-x-2">
             <a
               href={siteMetadata.linkedin}
               className="inline-block w-6 h-6 hover:scale-110 transition-transform ease duration-200"

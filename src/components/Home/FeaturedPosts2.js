dc4e22a316966c052d55749e6bbe591759f41527
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-17 22:53:17 +0100

    scc

--- src/components/Home/FeaturedPosts2.js
+++ src/components/Home/FeaturedPosts2.js
@@ -21,6 +21,11 @@ const FeaturedPosts2 = ({ blogs }) => {
       <article className="flex flex-row items-start justify-end sm:mx-10 relative h-[164vh] sm:h-[85vh]">
         <ConnectedSite />
         <section className="w-full mt-0 margin-right:11rem margin-left:-174px sm:mt-24 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center ">
+          <br />
+          <br />
+          <br />
+          <br />
+
           <h2 className="text-center w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
             --- TRENDING NEWS ---
           </h2>

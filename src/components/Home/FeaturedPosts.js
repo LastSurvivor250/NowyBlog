d0c31afdde1d9f4d1cff8fe0101417295bd1565e
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-28 13:00:38 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -47,6 +47,15 @@ const FeaturedPosts = ({ blogs }) => {
                 <article className="col-span-1 sm:col-span-1 row-span-1 relative">
                   <BlogLayoutFour blog={sortedBlogs[10]} />
                 </article>
+                <article className=" col-span-1 sm:col-span-1 row-span-1 relative">
+                  <BlogLayoutFour blog={sortedBlogs[17]} />
+                </article>
+                <article className="col-span-1 sm:col-span-1 row-span-1 relative">
+                  <BlogLayoutFour blog={sortedBlogs[16]} />
+                </article>
+                <article className="col-span-1 sm:col-span-1 row-span-1 relative">
+                  <BlogLayoutFour blog={sortedBlogs[15]} />
+                </article>
                 <br /> <br /> <br /> <br /> <br /> <br /> <br />
               </div>
             </div>

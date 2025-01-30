a111364ab5a55d4f49ed640565b8e260b17af127
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 12:30:08 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -10,31 +10,28 @@ const FeaturedPosts = ({ blogs }) => {
   return (
     <>
       <div className="mb-16 sm:mb-24">
-        <article className="flex flex-row items-start justify-center sm:justify-between sm:mx-10 relative min-h-[164vh] sm:min-h-[85vh]">
+        <article className="flex flex-row items-start justify-center sm:justify-between sm:mx-10 relative">
           <ConnectedSite />
 
           <section className="w-full mt-0 sm:mt-24 px-4 sm:px-10 md:px-24 sxl:px-32 flex flex-row items-start gap-6">
             {/* Left & Center Grid */}
-            <div className="w-full max-w-[1200px] grid grid-rows-[2fr_1fr] sm:grid-rows-[1fr] sm:grid-cols-[2fr_1fr] gap-6">
+            <div className="w-full max-w-[900px] grid grid-cols-1 sm:grid-cols-2 gap-6">
               <h2 className="text-center w-full font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light mb-8 sm:col-span-2">
                 --- SMMA SCHOOL ---
               </h2>
 
               {/* Top Section */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 row-span-1">
-                {/* BlogLayoutFour (Left) */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8">
                 <div className="w-full">
                   <BlogLayoutOne blog={sortedBlogs[11]} />
                 </div>
-
-                {/* BlogLayoutOne (Right) */}
                 <div className="w-full">
                   <BlogLayoutOne blog={sortedBlogs[3]} />
                 </div>
               </div>
 
               {/* Bottom Section */}
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-8 row-span-1">
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-8">
                 {[7, 5, 10].map((index) => (
                   <div key={index}>
                     <BlogLayoutFour blog={sortedBlogs[index]} />
@@ -44,9 +41,9 @@ const FeaturedPosts = ({ blogs }) => {
             </div>
 
             {/* Right Column */}
-            <div className="w-full sm:w-1/3 max-w-[300px] grid grid-rows-4 gap-6">
+            <div className="w-full sm:w-1/3 max-w-[300px] flex flex-col gap-6">
               {[18, 19, 20, 21].map((index) => (
-                <div key={index} className="w-full">
+                <div key={index} className="flex-1">
                   <BlogLayoutFour blog={sortedBlogs[index]} />
                 </div>
               ))}

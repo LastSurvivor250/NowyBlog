2fa1a5f5b9ad748004b283c66184b209c6e8683c
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 18:31:29 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -43,7 +43,7 @@ const FeaturedPosts = ({ blogs }) => {
                 ))}
               </div>
               <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6">
-                {[7, 5, 10].map((index) => (
+                {[11, 6, 2].map((index) => (
                   <div key={index} className="h-[200px] sm:h-[220px]">
                     {" "}
                     {/* Fixed height */}

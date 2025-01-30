4c7ca3650c776eb57c27c992df9f92eccb428761
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 16:11:14 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -14,14 +14,13 @@ const FeaturedPosts = ({ blogs }) => {
         <ConnectedSite />
 
         <section className="w-full mt-0 sm:mt-24 px-4 sm:px-6 md:px-12 sxl:px-16 flex flex-col sm:flex-row gap-6">
-          {/* Main Content */}
+          {/* Main Content (Left & Center) */}
           <div className="w-full sm:w-2/3 flex flex-col">
             <h2 className="text-center sm:text-left font-bold capitalize text-2xl md:text-3xl xl:text-4xl text-dark dark:text-light mb-6 sm:mb-8">
               --- SMMA SCHOOL ---
             </h2>
 
-            <div className="grid gap-4 sm:gap-6">
-              {/* Top Row */}
+            <div className="grid gap-4 sm:gap-6 h-full">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
                 {[11, 3].map((index) => (
                   <div key={index} className="aspect-[2/1]">
@@ -30,7 +29,6 @@ const FeaturedPosts = ({ blogs }) => {
                 ))}
               </div>
 
-              {/* Middle Row */}
               <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6">
                 {[7, 5, 10].map((index) => (
                   <div key={index} className="aspect-[4/3]">
@@ -41,13 +39,17 @@ const FeaturedPosts = ({ blogs }) => {
             </div>
           </div>
 
-          {/* Right Column - BlogLayoutFive Posts */}
-          <div className="w-full sm:w-1/3 flex flex-col gap-4 sm:gap-6 mt-6 sm:mt-[56px]">
-            {[18, 19, 17, 16].map((index) => (
-              <div key={index} className="aspect-[5/3]">
-                <BlogLayoutFive blog={sortedBlogs[index]} />
-              </div>
-            ))}
+          {/* Right Column - Adjusted Height */}
+          <div className="w-full sm:w-1/3 flex flex-col gap-4 sm:gap-6 mt-6 sm:mt-[56px] h-full">
+            <div className="grid grid-rows-4 gap-4 sm:gap-6 h-full">
+              {[18, 19, 17, 16].map((index) => (
+                <div key={index} className="aspect-[8/5]">
+                  {" "}
+                  {/* Changed from 5/3 */}
+                  <BlogLayoutFive blog={sortedBlogs[index]} />
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </article>

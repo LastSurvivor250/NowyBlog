d0b42b2bfbb58cd79fb6b7d48d67cc3402b553d6
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 10:57:51 +0100

    scc

--- src/components/Blog/BlogLayoutOne.js
+++ src/components/Blog/BlogLayoutOne.js
@@ -6,7 +6,7 @@ import { slug } from "github-slugger";
 
 const BlogLayoutOne = ({ blog }) => {
   return (
-    <div className="group inline-block overflow-hidden relative">
+    <div className="group inline-block overflow-hidden rounded-lg w-full h-96 sm:h-[28rem] relative">
       <div
         className="absolute inset-0 w-full h-full
           bg-gradient-to-b from-transparent from-0% to-dark/90  
@@ -19,21 +19,21 @@ const BlogLayoutOne = ({ blog }) => {
         alt={blog.title}
         width={blog.image.width}
         height={blog.image.height}
-        className="object-cover group-hover:scale-105 transition-all ease duration-300"
+        className="w-full h-full object-cover group-hover:scale-105 transition-all ease duration-300"
         sizes="(max-width: 1180px) 100vw, 50vw"
       />
 
-      <div className="absolute bottom-0 p-4 xs:p-6 sm:p-10  position-absolute top:0">
+      <div className="absolute bottom-0 p-4 xs:p-6 sm:p-10">
         <Tag
           link={`/categories/${slug(blog.tags[0])}`}
           name={blog.tags[0]}
-          className="px-6 text-xs  sm:text-sm py-1 sm:py-2 !border "
+          className="px-6 text-xs sm:text-sm py-1 sm:py-2 !border"
         />
         <Link href={blog.url} className="mt-6">
           <h2 className="font-bold capitalize text-sm xs:text-base sm:text-xl md:text-2xl text-light mt-2 sm:mt-4">
             <span
               className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] dark:from-accentDark/50 dark:to-accentDark/50
-                group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "
+                group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500"
             >
               {blog.title}
             </span>

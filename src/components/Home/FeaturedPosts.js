3e0fcc7d860fb9d571d70df9bcef612318ed3c1d
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 10:23:01 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -12,36 +12,40 @@ const FeaturedPosts = ({ blogs }) => {
   return (
     <>
       <div className="mb-16 sm:mb-24">
-        <article className="flex flex-row items-start justify-end sm:mx-10 relative min-h-[164vh] sm:min-h-[85vh]">
+        <article className="flex flex-row items-start justify-center sm:justify-end sm:mx-10 relative min-h-[164vh] sm:min-h-[85vh]">
           <ConnectedSite />
-          <section className="w-full mt-0 sm:mt-24 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center sm:-ml-[174px] mr-[11rem]">
+
+          <section className="w-full mt-0 sm:mt-24 px-4 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center">
             <h2 className="text-center w-full font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light mb-8">
               --- SMMA SCHOOL ---
             </h2>
 
-            <div className="grid gap-6">
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:mt-16">
-                <article className="sm:w-3/4">
+            <div className="grid gap-6 w-full max-w-[1600px] mx-auto">
+              {/* Top Section */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8">
+                <div className="sm:w-[85%] lg:w-[75%]">
                   <BlogLayoutFour blog={sortedBlogs[1]} />
-                </article>
-                <article className="sm:-ml-48 sm:w-[125%]">
+                </div>
+                <div className="sm:-translate-x-20 lg:-translate-x-24 xl:-translate-x-32">
                   <BlogLayoutOne blog={sortedBlogs[3]} />
-                </article>
+                </div>
               </div>
 
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+              {/* Middle Row */}
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-8">
                 {[7, 5, 10].map((index) => (
-                  <article key={index}>
+                  <div key={index}>
                     <BlogLayoutFour blog={sortedBlogs[index]} />
-                  </article>
+                  </div>
                 ))}
               </div>
 
-              <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
+              {/* Bottom Row */}
+              <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 sm:gap-8">
                 {[17, 16, 15, 14].map((index) => (
-                  <article key={index}>
+                  <div key={index}>
                     <BlogLayoutFour blog={sortedBlogs[index]} />
-                  </article>
+                  </div>
                 ))}
               </div>
             </div>

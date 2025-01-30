1dbf3a41fbad29d115858a2c3ae174392eb7ebbf
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2025-01-30 21:15:43 +0100

    scc

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -9,65 +9,47 @@ const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
 
   return (
-    <div className="mb-16 sm:mb-24">
-      <article className="flex flex-col sm:flex-row items-start sm:justify-between sm:mx-4 lg:mx-10 relative">
-        <ConnectedSite />
+    <div className="relative isolate overflow-hidden bg-gradient-to-b from-light/5 via-dark/5 to-light/5 dark:from-dark/5 dark:via-light/5 dark:to-dark/5">
+      <ConnectedSite />
 
-        <section className="w-full mt-0 sm:mt-24 px-4 sm:px-6 md:px-12 sxl:px-16 flex flex-col sm:flex-row gap-6">
-          {/* Main Content (Left & Center) */}
-          <div className="w-full sm:w-2/3 flex flex-col">
-            <h2 className="text-center sm:text-left font-bold capitalize text-2xl md:text-3xl xl:text-4xl text-dark dark:text-light mb-6 sm:mb-8">
-              --- SMMA SCHOOL ---
-            </h2>
+      <section className="container mx-auto px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <h2 className="sr-only">SMMA School</h2>
 
-            <div className="grid gap-4 sm:gap-6">
-              {/* Top Row */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
-                {[11, 3].map((index) => (
-                  <div key={index} className="h-[300px] sm:h-[350px]">
-                    {" "}
-                    {/* Fixed height */}
-                    <BlogLayoutOne blog={sortedBlogs[index]} />
-                  </div>
-                ))}
-              </div>
+        {/* Grid Layout */}
+        <div className="grid auto-rows-[minmax(300px,auto)] grid-cols-1 gap-6 sm:grid-cols-8 sm:gap-8 lg:gap-12">
+          {/* Hero Post (BlogLayoutOne) - Spans 4 columns */}
+          <article className="relative z-10 sm:col-span-4 sm:row-span-2">
+            <BlogLayoutOne blog={sortedBlogs[0]} />
+          </article>
 
-              {/* Middle Row */}
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6">
-                {[7, 5, 10].map((index) => (
-                  <div key={index} className="h-[200px] sm:h-[220px]">
-                    {" "}
-                    {/* Fixed height */}
-                    <BlogLayoutFour blog={sortedBlogs[index]} />
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6">
-                {[11, 6, 2].map((index) => (
-                  <div key={index} className="h-[200px] sm:h-[220px]">
-                    {" "}
-                    {/* Fixed height */}
-                    <BlogLayoutFour blog={sortedBlogs[index]} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+          {/* Featured Post (BlogLayoutFour) - Spans 4 columns with overlay */}
+          <article className="group relative sm:col-span-4">
+            <div className="absolute inset-0 z-0 bg-gradient-to-t from-dark/60 to-transparent" />
+            <BlogLayoutFour blog={sortedBlogs[1]} />
+          </article>
 
-          {/* Right Column - Matched Height */}
-          <div className="w-full sm:w-1/3 flex flex-col gap-4 sm:gap-[2.5rem] mt-6 sm:mt-[72px]">
-            {[18, 19, 17].map((index) => (
-              <div key={index} className="h-[200px] sm:h-[220px]">
-                {" "}
-                {/* Fixed height */}
-                <BlogLayoutFive blog={sortedBlogs[index]} />
-              </div>
-            ))}
-          </div>
-        </section>
-      </article>
+          {/* Vertical Posts (BlogLayoutFive) - Creates rhythm */}
+          {[2, 3, 4].map((index) => (
+            <article key={index} className="sm:col-span-2">
+              <BlogLayoutFive blog={sortedBlogs[index]} />
+            </article>
+          ))}
+
+          {/* Spotlight Post (BlogLayoutFour) - Creates visual break */}
+          <article className="relative sm:col-span-3 sm:col-start-6">
+            <div className="absolute -inset-4 -z-10 bg-accent/10" />
+            <BlogLayoutFour blog={sortedBlogs[5]} />
+          </article>
+
+          {/* Grid Filler Posts (BlogLayoutFive) - Maintains rhythm */}
+          {[6, 7, 8].map((index) => (
+            <article key={index} className="sm:col-span-2">
+              <BlogLayoutFive blog={sortedBlogs[index]} />
+            </article>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
-
 export default FeaturedPosts;
